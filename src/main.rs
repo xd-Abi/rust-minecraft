c2a82813526c0df40f8f9904420673a1c97f21d1
xd-Abi <abinayansureskumar@gmail.com>
2023-11-26 14:44:53 +0100

    Added basic glfw window and displaying it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    use glfw::fail_on_errors;
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    let (mut window, events) = glfw.create_window(
+        1280, 
+        720,
+        "Rust Minecraft | by xdAbi", 
+        glfw::WindowMode::Windowed
+    ).expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers();
+        glfw.poll_events();
+    }
 }
